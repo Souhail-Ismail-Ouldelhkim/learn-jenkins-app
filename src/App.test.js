16e3094fd94e39f6d9d6567c13e05dbd27702159
Souhail-Ismail-Ouldelhkim <souhailouldelhkim9@gmail.com>
2026-03-12 15:04:46 +0000

    zszs

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn Jenkins link', () => {
+test("renders learn Jenkins link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn Jenkins/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// Unit Test for App component
